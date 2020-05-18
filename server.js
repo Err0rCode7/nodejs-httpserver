2919bfe0604dd058c7827ca2ac672ad29abaf7c5
Err0rCode7 <aa6184708@naver.com>
2020-05-18 22:28:07 +0900

    # routing with httpserver and routing with express

---
+++ server.js
@@ -0,0 +1,33 @@
+// require 은 import와 비슷한 개념으로 객체, 변수 등을 가져온다.
+const express = require('express');
+// http server 생성 요청과, 응답을 기본 파라미터로 한다.
+
+const app = express();
+
+app.get('/', (req,res,next) => {
+   console.log("url : ./");
+   res.json({url:'./', text:'It\'s first time'});
+});
+
+/*
+const server = http.createServer( (request, response) => {
+    const { headers, method, _url:url } = request;
+
+    response.writeHead(200, {'Content-Type':'text/html'});
+    response.end('Hello node.js!!');
+
+    if ( _url === '/'){
+        console.log("url : l");
+    }
+    else if ( _url === '/hi'){
+        console.log("url : ", url);
+    }
+});
+
+server.listen(8080, () => {
+    console.log('server is running');
+});
+*/
+app.listen(8080, () => {
+    console.log('express server is running');
+});
