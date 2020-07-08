f31b2e9fa51ba98a49821281ef4d8de367d89b95
Err0rCode7 <aa6184708@naver.com>
2020-07-09 01:03:05 +0900

    # Add routers (Get - commentList, Post - Comment and Reply, Delete - Comment and Reply)

--- routes/comment.js
+++ routes/comment.js
@@ -6,22 +6,131 @@ const config = require('../config/config.js');
 const router = express.Router();
 const pool = mysql.createPool(config.db());
 
-router.post('/', async (req, res) => {
+router.get('/list/:capsuleId', async (req, res) => {
 
-    console.log("request Ip ( Post Comment ) :",req.connection.remoteAddress.replace('::ffff:', ''));
+    console.log("request Ip ( Get Comment List ) :",req.connection.remoteAddress.replace('::ffff:', ''));
 
-    const {user_id, capsule_id, reply_flag} = req.body;
+    const capsule_id = req.params.capsuleId;
+    const selectCommentListQuery = `select \
+                                    c.nick_name as parent_nick_name, \
+                                    r.nick_name as child_nick_name, \
+                                    c.comment as parent_comment, \
+                                    r.comment as child_comment \
+                                    from comment as c \
+                                    LEFT JOIN reply r \
+                                    ON c.id = r.parent_id \
+                                    where c.capsule_id = ${capsule_id} \
+                                    ORDER BY c.id , \
+                                    r.id;`;
 
-    const createCommentQuery = `insert into comment (user_id, capsule_id, comment, reply_flag, date_created ) \
-                                values('${user_id}', ${capsule_id}, '${comment}', ${reply_flag}, now());`;
-    
     let conn;
 
     try {
 
         conn = await pool.getConnection();
 
-        if (user_id == undefined || capsule_id == undefined || comment == undefined || reply_flag == undefined) {
+        if (capsule_id == undefined) {
+            throw "Comment Exception : Undefined Request Body";
+        }
+        
+        await conn.beginTransaction();
+
+        const resultSelectCommentListQuery = await conn.query(selectCommentListQuery);
+        const rowCommentList = resultSelectCommentListQuery[0];
+
+        let count = 0;
+
+        await conn.commit();
+
+        let childList = [];
+        let commentList = [];
+        let temp_parent_nick;
+        let temp_parent_comment;
+        rowCommentList.forEach( item => {
+
+            const {parent_nick_name, child_nick_name, parent_comment, child_comment} = item;
+
+            if (count == 0){
+                // No Reply
+                childList = [];
+                if (child_nick_name == null){
+                    commentList.push({
+                        nick_name: parent_nick_name,
+                        comment: parent_comment,
+                        replies: childList
+                    });
+                } else {
+                    temp_parent_nick = parent_nick_name;
+                    temp_parent_comment = parent_comment;
+                    childList.push({
+                        nick_name: child_nick_name,
+                        comment: child_comment
+                    });
+                    count++;
+                }
+            } else {
+                if (child_nick_name == null){
+                    commentList.push({
+                        nick_name: temp_parent_nick,
+                        comment: temp_parent_comment,
+                        replies: childList
+                    });
+                    commentList.push({
+                        nick_name: parent_nick_name,
+                        comment: parent_comment,
+                        replies: []
+                    })
+                    count = 0;
+
+                } else {
+                    temp_parent_nick = parent_nick_name;
+                    temp_parent_comment = parent_comment;
+                    childList.push({
+                        nick_name: child_nick_name,
+                        comment: child_comment
+                    });
+                    count++; 
+                }
+            }
+
+        });
+        res.writeHead(200, {'Content-Type':'application/json'});
+        res.end(JSON.stringify(commentList));
+
+    } catch (e) {
+
+        console.log(e)
+
+        await conn.rollback();
+        res.writeHead(404, {'Content-Type':'application/json'});
+        res.end();
+
+    } finally {
+
+        conn.release();
+
+    }
+
+});
+
+router.post('/', async (req, res) => {
+
+    console.log("request Ip ( Post Comment ) :",req.connection.remoteAddress.replace('::ffff:', ''));
+
+    const {user_id, nick_name, capsule_id, comment, parent_id} = req.body;
+
+    const createCommentQuery = `insert into comment (user_id, capsule_id, comment, nick_name, date_created ) \
+                                values('${user_id}', ${capsule_id}, '${comment}', '${nick_name}', now());`;
+    const createChildCommentQuery = `insert into reply (user_id, capsule_id, parent_id, comment, nick_name, date_created ) \
+                                values('${user_id}', ${capsule_id}, ${parent_id} ,'${comment}', '${nick_name}', now());`;
+    let conn;
+    let query;
+
+    try {
+
+        conn = await pool.getConnection();
+
+        if (user_id == undefined || capsule_id == undefined || comment == undefined || nick_name == undefined) {
             throw "Comment Exception : Undefined Request Body";
         }
 
@@ -29,9 +138,15 @@ router.post('/', async (req, res) => {
             throw "Commnet Exception : Comment's length is too long ";
         }
 
+        if (parent_id == undefined) {
+            query = createCommentQuery;        
+        } else {
+            query = createChildCommentQuery;
+        }
+
         await conn.beginTransaction();
 
-        const resultCreateComment = await conn.query(createCommentQuery);
+        const resultCreateComment = await conn.query(query);
 
         const rowCreateComment = resultCreateComment[0];
 
@@ -59,12 +174,12 @@ router.post('/', async (req, res) => {
 
 });
 
-router.post('/deleting', async (req, res) => {
+router.delete('/:id', async (req, res) => {
 
-    console.log("request Ip ( Post Deleting Comment ) :",req.connection.remoteAddress.replace('::ffff:', ''));
+    console.log("request Ip ( Deleting Comment ) :",req.connection.remoteAddress.replace('::ffff:', ''));
 
-    const {user_id, comment_id} = req.body;
-    const deleteCommnetQuery = `delete comment where user_id = '${user_id}' and comment_id = ${comment_id};`;
+    const id = req.params.id;
+    const deleteCommentQuery = `delete from comment where id = ${id};`;
 
     let conn;
 
@@ -72,17 +187,64 @@ router.post('/deleting', async (req, res) => {
 
         conn = await pool.getConnection();
 
-        if (user_id == undefined || capsule_id == undefined || comment == undefined || reply_flag == undefined) {
+        if (id == undefined) {
             throw "Comment Exception : Undefined Request Body";
         }
         
         await conn.beginTransaction();
 
-        const resultDeleteComment = await conn.query(deleteCommnetQuery);
+        const resultDeleteComment = await conn.query(deleteCommentQuery);
         const rowDeleteComment = resultDeleteComment[0];
 
         if (rowDeleteComment.affectedRows == 0){
-            throw "Follow Exception : Cant Delete Comment";
+            throw "Follow Exception : Comment not exist";
+        }
+        
+        await conn.commit();
+
+        res.writeHead(200, {'Content-Type':'application/json'});
+        res.end('{"success": true}');
+
+    } catch (e) {
+
+        console.log(e)
+
+        await conn.rollback();
+        res.writeHead(404, {'Content-Type':'application/json'});
+        res.end();
+
+    } finally {
+
+        conn.release();
+
+    }
+
+});
+
+router.delete('/reply/:id', async (req, res) => {
+
+    console.log("request Ip ( Delete Reply Comment ) :",req.connection.remoteAddress.replace('::ffff:', ''));
+
+    const id = req.params.id;
+    const deleteReplyQuery = `delete from reply where id = ${id};`;
+
+    let conn;
+
+    try {
+
+        conn = await pool.getConnection();
+
+        if (id == undefined) {
+            throw "Comment Exception : Undefined Request Body";
+        }
+        
+        await conn.beginTransaction();
+
+        const resultDeleteReply = await conn.query(deleteReplyQuery);
+        const rowDeleteReply = resultDeleteReply[0];
+
+        if (rowDeleteReply.affectedRows == 0){
+            throw "Follow Exception : Reply Not exist";
         }
         
         await conn.commit();
