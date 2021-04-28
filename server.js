d1ed9745b4077b87575ae4c2188f71e321c9c67a
Err0rCode7 <jeon4708@gmail.com>
2021-04-29 04:22:22 +0900

    # Remove comments

--- server.js
+++ server.js
@@ -64,37 +64,6 @@ app.use('/session', sessionRouter);
 //app.use('/videos', videoRouter);
 
 
-/* 
-// 라우터 get 방식 예시
-app.get('/', (req,res,next) => {
-   console.log("url : ./");
-   res.json({url:'./', text:'It\'s first time'});
-});
-*/
-
-/*
-// 일반 nodejs 버전
-// http server 생성 요청과, 응답을 기본 파라미터로 한다.
-const server = http.createServer( (request, response) => {
-    const { headers, method, _url:url } = request;
-
-    response.writeHead(200, {'Content-Type':'text/html'});
-    response.end('Hello node.js!!');
-    
-    // express router 사용안했을 때 routing 로직
-    if ( _url === '/'){
-        console.log("url : l");
-    }
-    else if ( _url === '/hi'){
-        console.log("url : ", url);
-    }
-});
-
-server.listen(8080, () => {
-    console.log('server is running');
-});
-*/
-
 app.listen(config.url().port, () => {
     console.log('express server is running');
 });
